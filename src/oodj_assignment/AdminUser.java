4f40e7bb7260e7b394ca82e06ee13c0b8818c3f2
WONG JIE SHENG <TP056464@mail.apu.edu.my>
2021-08-01 12:13:38 +0800

    added AdminUser class

---
+++ src/oodj_assignment/AdminUser.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oodj_assignment;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author asus
+ */
+public class AdminUser extends User
+{
+    String filePath = "C:\\Users\\wongj\\OneDrive - Asia Pacific University\\Desktop\\User.txt";
+    String role = "Admin"; //This role can change
+    public boolean login(String username, String password) throws IOException
+    {
+        return verifyLogin(filePath, "Admin", username, password);
+    }
+    
+    public void addAdmin() throws IOException
+    {
+        addUser(filePath, role);
+    }
+    
+    public void deleteAdmin() throws IOException
+    {   
+        String newUsername = null;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter username of the user to delete: ");
+        newUsername = sc.nextLine();
+        while(!checkUserExist(filePath, role, newUsername))
+        {
+            System.out.println("User not found! Please try again");
+            System.out.println("Enter username of the user to delete: ");
+            newUsername = sc.nextLine();
+        }
+        deleteUser(filePath, role, newUsername); 
+    }
+    
+    public void editAdmin() throws IOException
+    {   
+        String newUsername = null;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter username of the user to edit: ");
+        newUsername = sc.nextLine();
+        while(!checkUserExist(filePath, role, newUsername))
+        {
+            System.out.println("User not found! Please try again");
+            System.out.println("Enter username of the user to edit: ");
+            newUsername = sc.nextLine();
+        }
+        editUser(filePath, role, newUsername); 
+         
+    }
+    
+    public void viewAdmin() throws IOException
+    {   
+        viewUser(filePath, role);   
+    }
+    public void searchAdmin() throws IOException
+    {   
+        String newUsername = null;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter username of the user: ");
+        newUsername = sc.nextLine();
+        searchUser(filePath, role, newUsername);      
+    }
+}
+
